cb522d7c89d4d185ebbd89f1033b58844237db2a
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2021-03-24 09:32:00 +0100

    moved functions from task3/utils to utils/utils

--- utils/utils.py
+++ utils/utils.py
@@ -26,7 +26,6 @@ def run_episode(env, policy, stumble_probability, print_iter=False):
     return iterations, total_reward
 
 
-
 def mlp(sizes, activation, output_activation=nn.Identity):
     layers = []
     for j in range(len(sizes)-1):
@@ -35,10 +34,10 @@ def mlp(sizes, activation, output_activation=nn.Identity):
     return nn.Sequential(*layers)
 
 
-def convert_state(state, env_size, norm, device):
+def convert_state(observation, env_size = 10, norm = 2., device = "cpu"):
     # Adapted from lab 7 feedback
     """
-    state (dict) : dictionary with relative coordinates and agent's surroundings
+    observation (dict) : dictionary with relative coordinates and agent's surroundings
     
     env_size (int) : length of the environment's sides
     
@@ -47,10 +46,11 @@ def convert_state(state, env_size, norm, device):
     device (torch object) : GPU or CPU
     
     """
-    rel_coor_car = state['relative_coordinates_car'].flatten()/env_size
-    rel_coor_britney = state['relative_coordinates_britney'].flatten()/env_size
-    o = state['surroundings'].flatten()/norm
+    rel_coor_car = observation['relative_coordinates_car'].flatten()/env_size
+    rel_coor_britney = observation['relative_coordinates_britney'].flatten()/env_size
+    o = observation['surroundings'].flatten()/norm
     state_tensor = np.concatenate([rel_coor_car, rel_coor_britney, o])
-    state_tensor = torch.tensor(state_tensor, device=device).unsqueeze(0)
+    #state_tensor = torch.tensor(state_tensor).unsqueeze(0)
+    state_tensor = torch.tensor(state_tensor, dtype=torch.float32).unsqueeze(0)
     
     return state_tensor
