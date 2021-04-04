bab54c6b223ad550672b32c20b6ebc4c47066fd3
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2021-04-04 16:32:21 +0200

    first iteration of asspapsworld.py - not tested yet

--- task1/asspapsworld.py
+++ task1/asspapsworld.py
@@ -22,12 +22,10 @@ class AssPapWorld(ass.AssassinWorld):
         
         # Reward received by agent if Britney is photographed
         self.photo_reward = photo_reward
-        
-
-        
+    
         # Create paparazzi
         self.paps_generator = Paparazzi()
-        self.paparazzi= []
+        self.paparazzi = []
         for i in range(num_paps):
             self.paparazzi.append(self.paps_generator())
         
@@ -46,6 +44,72 @@ class AssPapWorld(ass.AssassinWorld):
         self.map[self.peephole, -3] = 0
         
 
+                
+
+class Paparazzi:
+    
+    """ One of these objects represents one paparazzi """
+    
+    def __init__(self, env_size, photo_reward):
+        self.location, self.travel_direction = self.create_pap(env_size)
+        self.direction_arr = self.create_directional_arr(self.travel_direction)
+        self.photo_reward = photo_reward
+        
+    def create_pap(self, env_size):
+        """
+        Creates an initial location and a travel direction for the paparazz(o/a)
+        The pap can only travel along one axis and starts by traveling in
+        a randomly selected direction along that axis. The axis along which 
+        the pap can travel is determined by its initial location.
+        """
+        x_loc = random.randint(0, env_size-3) # -3 because the environment's outer rim are barriers
+        
+        if x_loc == 0 or x_loc == env_size-3:
+            y_loc = random.randint(1, env_size-4)
+            travel_direction = random.choice(["E", "W"]) # east / west
+        else:
+            y_loc = random.choice([0, env_size-3])
+            travel_direction = random.choice(["N", "S"]) # north / south
+        
+        location = np.array([x_loc, y_loc])
+        
+        return location, travel_direction
+    
+    def create_directional_arr(self, travel_direction):
+        """Creating array that will move paps north, south, east or west"""
+        if travel_direction == "E":
+            return np.array([0, 1])
+        if travel_direction == "W":
+            return np.array([0, -1])
+        if travel_direction == "N":
+            return np.array([1, 0])
+        if travel_direction == "S":
+            return np.array([-1, 0])         
+        
+    def move_pap(self, env_map):
+        """
+        Method moves a pap. It creates a new location. If new location is a
+        barrier, turn around and travel the other way. Else, continue in current
+        direction.
+        """
+        new_location = self.location + self.direction_arr
+        if env_map[new_location[0], new_location[1]] == 1:
+            self.travel_direction = self.change_direction(self.travel_direction)
+            self.direction_arr = self.create_directional_arr(self.travel_direction)
+            self.location += self.direction_arr
+        else:
+            self.location = new_location
+         
+    def change_direction(self, current_direction):
+        if current_direction == "E":
+            return "W"
+        if current_direction == "W":
+            return "E"
+        if current_direction == "N":
+            return "S"
+        if current_direction == "S":
+            return "N"
+
         
     def take_photo(self, pap, photo_reward):
         """
@@ -60,64 +124,17 @@ class AssPapWorld(ass.AssassinWorld):
                 return self.photo_reward
         else:
             return 0
-        
+
     def britney_in_sight(self, britney_location, pap_location):
         if britney_location[0] == pap_location[0] \
             or britney_location[1] == pap_location[1]:
                 return True
         else:
             return False
-                
 
-class Paparazzi:
-    """ Generates paparazzi objects """
-    
-    def __init__(self, env_size):
-        
-        # Creating arrays that will move paps north, south, east and west
-        
-        self.south_arr = np.array([-1, 0])
-        self.east_arr = np.array([0, 1])
-        self.west_arr = np.array([0, -1])
-        
-        self.location, self.travel_direction = self.create_pap(env_size)
-        
-        self.direction_arr = self.create_directional_arr()
-        
-        
-    def create_pap(self, env_size):
-        """
-        Creates an initial location and a travel direction for the paparazz(o/a)
-        The pap can only travel along one axis and starts by traveling in
-        a randomly selected direction along that axis. The axis along which 
-        the pap can travel is determined by its initial location.
-        """
 
-        x_loc = random.randint(0, env_size-3) # -3 because the environment's outer rim are barriers
-        
-        if x_loc == 0 or x_loc == env_size-3:
-            y_loc = random.randint(1, env_size-4)
-            travel_direction = random.choice(["E", "W"]) # east / west
-            
-        else:
-            y_loc = random.choice([0, env_size-3])
-            travel_direction = random.choice(["N", "S"]) # north / south
-        
-        location = np.array([x_loc, y_loc])
-        
-        return location, travel_direction
-    
-    def create_directional_arr(self):
-        """Creating array that will move paps north, south, east and west"""
-        if self.travel_direction == "E":
-            return np.array([0, 1])
-        if self.travel_direction == "W":
-            return np.array([0, -1])
-        if self.travel_direction == "N":
-            return np.array([1, 0])
-        if self.travel_direction == "S":
-            return np.array([-1, 0])         
-        
-    def move_pap(self):
-        self.location += self.direction_arr   
+
+
+
+
     
