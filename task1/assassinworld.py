9ea175eb04a272441de0297108998eb40964c657
Kasper Groes Ludvigsen <groes.ludvigsen@gmail.com>
2021-04-05 20:43:11 +0200

    finished making and testing ass + pap envs

--- task1/assassinworld.py
+++ task1/assassinworld.py
@@ -7,7 +7,7 @@ Created on Fri Mar 12 12:15:33 2021
 
 import task1.britneyworld as bw
 import numpy as np
-
+import random
     
 # TEST
 
@@ -16,12 +16,17 @@ class AssassinWorld(bw.Environment):
     assassin_locations : 2d numpy array that contains a number of (x,y) coordinates
     
     """
-    def __init__(self, N, stumble_prob, assassin_locations):
-        super().__init__(N, stumble_prob)
+    def __init__(self, environment_params, obs_size, assassin_locations,
+                 stab_probability, stab_reward):
+        super().__init__(environment_params, obs_size)
 
         self.assassin_locations = assassin_locations
         self.num_assassins = len(assassin_locations)
         self.reward = 0
+        # Probability that agent is stabbed if adjacent to assassin
+        self.stab_probability = stab_probability
+        # Negative reward (int) received if agent or britney is stabbed
+        self.stab_reward = stab_reward
         
     def __call__(self, action):
         done = False
@@ -38,18 +43,42 @@ class AssassinWorld(bw.Environment):
         return self.reward, done
         
     def move_assassins(self, britney_location, assassin_locations):
-        return assassin_locations + np.sign(britney_location-assassin_locations)
+        if random.random() > 0.5:
+            return assassin_locations + np.sign(britney_location-assassin_locations)
+        return assassin_locations
         
-    def stab(self, britney_location, assassin_locations):
+    def stab(self, britney_location, assassin_locations, guard_location):
+        reward = 0
+        done = False
         for loc in assassin_locations:
-            if self.are_locations_adjacent(britney_location, loc):
-                done = True
-                reward = -20
-            else:
-                done = False
-                reward = 0
-                
+            britney_stabbed, r = self.stab_britney(britney_location, loc)
+            reward += r
+            guard_stabbed, r = self.stab_guard(guard_location, loc)
+            reward += r
+            if guard_stabbed or britney_stabbed:
+                done = True 
         return done, reward
+    
+    def stab_britney(self, britney_location, assassin_location):
+        if self.are_locations_adjacent(britney_location, assassin_location):
+            done = True
+            print("Britney got stabbed")
+            return done, self.stab_reward
+        done = False
+        reward = 0
+        return done, reward
+    
+    def stab_guard(self, guard_location, assassin_location):
+        if self.are_locations_adjacent(guard_location, assassin_location) \
+            and random.random() < self.stab_probability:
+                print("Guard got stabbed")
+                done = True
+                return done, self.stab_reward
+        done = False
+        reward = 0
+        return done, reward
+        
+        
             
 
 
--- task1/asspapsworld.py
+++ task1/asspapsworld.py
@@ -9,6 +9,10 @@ import task1.assassinworld as ass
 import numpy as np
 import random
 
+
+def get_peephole(env_size):
+    return env_size//2
+
 class AssPapWorld(ass.AssassinWorld):
     """
     Class for generating env with paparazzi. If papparazzis get to take
@@ -17,17 +21,20 @@ class AssPapWorld(ass.AssassinWorld):
     that are placed roughly in the middle of an axis
     """
     
-    def __init__(self, N, stumble_prob, assassin_locations, num_paps, photo_reward):
-        super().__init__(N, stumble_prob, assassin_locations)
+    def __init__(self, environment_params, obs_size, assassin_locations,
+                 stab_probability, stab_reward, num_paps, photo_reward):
+        
+        super().__init__(environment_params, obs_size, assassin_locations,
+                         stab_probability, stab_reward)
         
         # Reward received by agent if Britney is photographed
         self.photo_reward = photo_reward
     
         # Create paparazzi
-        self.paps_generator = Paparazzi()
+        #self.paps_generator = Paparazzi(self.size, photo_reward)
         self.paparazzi = []
         for i in range(num_paps):
-            self.paparazzi.append(self.paps_generator())
+            self.paparazzi.append(Paparazzi(self.size, photo_reward))
         
         # Create barriers for paparazzi - not technically necessary, but we do
         # it to be able to visualize the barriers
@@ -37,14 +44,35 @@ class AssPapWorld(ass.AssassinWorld):
         self.map[:,-3] = 1
         
         # Making peepholes in the middle through which paps can take photos
-        self.peephole = self.size//2
+        self.peephole = get_peephole(self.size)
         self.map[2, self.peephole] = 0
         self.map[-3, self.peephole] = 0
         self.map[self.peephole, 2] = 0
         self.map[self.peephole, -3] = 0
         
-
-                
+    def environment_step(self, action):
+        done = False
+        
+        # Moving assassins and stabbing
+        done, reward = self.stab(self.britney_location, self.assassin_locations,
+                                 self.guard_location)
+        self.reward += reward
+        self.assassin_locations = self.move_assassins(self.britney_location,
+                                                      self.assassin_locations)
+        
+        # Paps take photos and move
+        for pap in self.paparazzi:
+            photo_reward = pap.take_photo(self.guard_location, self.britney_location)
+            pap.move_pap(self.map)
+            self.reward += photo_reward
+        
+        if not done:
+            _, reward, done = self.take_action_guard(self.guard_location,
+                                                         self.britney_location, action)
+            self.reward += reward
+        
+        return self.reward, done
+        
 
 class Paparazzi:
     
@@ -54,6 +82,7 @@ class Paparazzi:
         self.location, self.travel_direction = self.create_pap(env_size)
         self.direction_arr = self.create_directional_arr(self.travel_direction)
         self.photo_reward = photo_reward
+        self.peephole = get_peephole(env_size)
         
     def create_pap(self, env_size):
         """
@@ -62,14 +91,36 @@ class Paparazzi:
         a randomly selected direction along that axis. The axis along which 
         the pap can travel is determined by its initial location.
         """
-        x_loc = random.randint(0, env_size-3) # -3 because the environment's outer rim are barriers
         
-        if x_loc == 0 or x_loc == env_size-3:
-            y_loc = random.randint(1, env_size-4)
-            travel_direction = random.choice(["E", "W"]) # east / west
+        # 1 = top row, 2 = rightmost col, 3 = buttom row, 4 = leftmost col
+        self.side = random.choice([1,2,3,4]) 
+        
+        if self.side == 1:
+            x_loc = 1
+            y_loc = random.randint(3, env_size-4)
+            travel_direction = random.choice(["E", "W"])
+        elif self.side == 2:
+            x_loc = random.randint(3, env_size-4)
+            y_loc = env_size-2
+            travel_direction = random.choice(["S", "N"])
+        elif self.side == 3:
+            x_loc = env_size-2
+            y_loc = random.randint(3, env_size-4)
+            travel_direction = random.choice(["E", "W"])
         else:
-            y_loc = random.choice([0, env_size-3])
-            travel_direction = random.choice(["N", "S"]) # north / south
+            x_loc = random.randint(3, env_size-4)
+            y_loc = 1
+            travel_direction = random.choice(["S", "N"])
+        
+        
+        #x_loc = random.randint(0, env_size-3) # -3 because the environment's outer rim are barriers
+        
+        #if x_loc == 1 or x_loc == env_size-3:
+        #    y_loc = random.randint(1, env_size-4)
+        #    travel_direction = random.choice(["E", "W"]) # east / west
+        #else:
+        #    y_loc = random.choice([0, env_size-3])
+        #    travel_direction = random.choice(["N", "S"]) # north / south
         
         location = np.array([x_loc, y_loc])
         
@@ -82,15 +133,15 @@ class Paparazzi:
         if travel_direction == "W":
             return np.array([0, -1])
         if travel_direction == "N":
-            return np.array([1, 0])
+            return np.array([-1, 0])
         if travel_direction == "S":
-            return np.array([-1, 0])         
+            return np.array([1, 0])         
         
     def move_pap(self, env_map):
         """
-        Method moves a pap. It creates a new location. If new location is a
-        barrier, turn around and travel the other way. Else, continue in current
-        direction.
+        Method moves a pap. It creates a new location and assigns this as 
+        self.location. If new location is a barrier, turn around and travel 
+        the other way. Else, continue in current direction.
         """
         new_location = self.location + self.direction_arr
         if env_map[new_location[0], new_location[1]] == 1:
@@ -111,30 +162,38 @@ class Paparazzi:
             return "N"
 
         
-    def take_photo(self, pap, photo_reward):
+    def take_photo(self, guard_location, britney_location):
         """
         If pap (Paparazzi object) is by peephole and britney is in sight (i.e.
         in same col/row), the agent receives a negative reward equal to 
         photo_reward (int)
         """
-        
-        if np.any(pap.location == self.peephole):
-            britney_got_papped = self.britney_in_sight(pap.location)#and np.any(self.britney_location == self.peephole):
-            if britney_got_papped:
-                return self.photo_reward
-        else:
-            return 0
+        if self.pap_by_peephole() \
+            and self.britney_in_sight(britney_location) \
+            and not self.is_guard_blocking(guard_location, britney_location):
+            return self.photo_reward
+        return 0
 
-    def britney_in_sight(self, britney_location, pap_location):
-        if britney_location[0] == pap_location[0] \
-            or britney_location[1] == pap_location[1]:
-                return True
-        else:
-            return False
+    def britney_in_sight(self, britney_location):
+        """ Checks if Britney is in the same column or row as the pap """
+        if np.any(britney_location == self.peephole):
+            return True
+        return False
 
 
+    def pap_by_peephole(self):
+        if np.any(self.location == self.peephole):
+            return True
+        return False
 
 
-
-
-    
+    def is_guard_blocking(self, guard_location, britney_location):
+        if self.side == 1 and guard_location[1] == self.peephole and guard_location[0] < britney_location[0]:
+            return True
+        if self.side == 2 and guard_location[0] == self.peephole and guard_location[1] > britney_location[1]:
+            return True
+        if self.side == 3 and guard_location[1] == self.peephole and guard_location[0] > britney_location[0]:
+            return True  
+        if self.side == 4 and guard_location[0] == self.peephole and guard_location[1] < britney_location[1]:
+            return True
+        return False
